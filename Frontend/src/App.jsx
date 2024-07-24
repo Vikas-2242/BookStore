d8a37cedba5ca850d7735c5ff54cc19db94a5831
vikas-2242 <vikasverma151522001@gmail.com>
2024-07-25 01:30:47 +0530

    Backend Done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,60 @@
-import React from 'react'
-import Home from './home/Home';
-import { Route, Routes } from "react-router-dom";
-import Courses from './courses/Courses';
-import Signup from './component/Signup';
+// import { Navigate, Route, Routes } from "react-router-dom";
+// import Courses from './courses/Courses';
+// import Signup from './component/Signup';
+// // import Home from './home/Home';
+// import { Toaster } from "react-hot-toast";
+// import { useAuth } from "./context/AuthProvider";
+
+// // import { Route, Routes } from "react-router-dom";
+
+
+// import {Toaster} from "react-hot-toast"
+// // import { useAuth } from "./context/AuthProvider";
+// function App() {
+//   return(
+//   <>
+// <div className='dark:bg-slate-900 dark:text-white'>
+//   <Routes>
+//   <Route path="/" element={<Home/>}/>
+//   <Route path='/course'
+//   element={authUser ? <Courses /> : <Navigate to="/signup" />} />
+//   <Route path='/signup' element={<Signup/>}/>
+//  </Routes>
+//  <Toaster />
+// </div>
+
+//   </>);
+
+// }
+// export default App;
+
+
+import React from "react";
+import Home from "./home/Home";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Courses from "./courses/Courses";
+import Signup from "./component/Signup";
+import { Toaster } from "react-hot-toast";
+import { useAuth } from "./context/AuthProvider";
+
 function App() {
-  return(
-  <>
-
-<div className='dark:bg-slate-900 dark:text-white'>
-  <Routes>
-  <Route path="/" element={<Home/>}/>
-  <Route path='/course' element={<Courses/>}/>
-  <Route path='/signup' element={<Signup/>}/>
- </Routes>
-</div>
-
-  </>);
-
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
+  return (
+    <>
+      <div className="dark:bg-slate-900 dark:text-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/course"
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Toaster />
+      </div>
+    </>
+  );
 }
+
 export default App;
--- Frontend/src/component/freebook.jsx
+++ Frontend/src/component/freebook.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import axios from "axios"
 
-import list from"../../public/list.json";
+// import list from"../../public/list.json";
 import Cards from './Cards';
 
 
 function freebook() {
-    const filterdata=list.filter((data)=>data.category==="Free");
+  const [book,setBook]=useState([]);
+  useEffect(()=>{
+    const getBook=async()=>{
+      try {
+        const res=await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        const data =res.data.filter((data)=>data.category==="Free")
+        setBook(data);
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getBook();
+  },[])
     var settings = {
         dots: true,
         infinite: false,
@@ -52,7 +67,7 @@ function freebook() {
   </div>
  <div>
  <Slider {...settings}>
-       {filterdata.map((item)=>(
+       {book.map((item)=>(
         <Cards item={item} key={item.id}/>
        ))}
         </Slider>
